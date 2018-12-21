32b11036bc3796f1dd9d8620fd51dda6ba8b7202
jaguso <jagutzo@gmail.com>
2018-12-20 18:31:31 -0600

    navbar made

---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div className="row justify-content-center">
+        <h3>This is Sparta biatch!!!</h3>
+      </div>
+    );
+  }
+}
+
+export default Signup;
